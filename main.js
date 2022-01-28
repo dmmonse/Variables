5e03601921dad61f05e0dbee7218a2322d934d89
monse <78182277+dmmonse@users.noreply.github.com>
2022-01-27 23:53:53 -0600

    variables added
    global variable
    block variable
    string  variable

---
+++ main.js
@@ -0,0 +1,13 @@
+//global variable//
+var x=3
+var y=6
+
+//block variable//
+{
+    let x=2
+}
+
+//string varible//
+let firstName= "guys"
+let lastName= "to my assignmnet"
+let text =`Welcome ${firstName}, ${lastName}!`;
