715785eebd1f28b3aa301678ba33a98658689ac5
monse <78182277+dmmonse@users.noreply.github.com>
2022-01-27 23:56:51 -0600

    object variable
    object variable added

--- main.js
+++ main.js
@@ -5,9 +5,15 @@ var y=6
 //block variable//
 {
     let x=2
-}
+};
 
 //string varible//
 let firstName= "guys"
 let lastName= "to my assignmnet"
 let text =`Welcome ${firstName}, ${lastName}!`;
+
+//object variable//
+const professor = {
+    firstName: "Rodolfo",
+    lastName: "Macias",
+};
