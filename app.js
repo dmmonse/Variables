e2fefda7836c3e58f509d468c3bff74fc464b47c
monse <78182277+dmmonse@users.noreply.github.com>
2022-01-28 00:03:35 -0600

    changed variable for string
    from 'text' to 'hey'

---
+++ app.js
@@ -0,0 +1,19 @@
+//global variable//
+var x=3
+var y=6
+
+//block variable//
+{
+    let z=2
+}
+
+//string varible//
+let firstName= "guys"
+let lastName= "to my assignmnet"
+let hey =`Welcome ${firstName}, ${lastName}!`;
+
+//object variable//
+const professor = {
+    firstName: "Rodolfo",
+    lastName: "Macias",
+}
